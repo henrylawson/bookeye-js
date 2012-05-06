cc35577e501c02e1700b8b878fcc181b42854792
Henry Lawson <henry.lawson@foinq.com>
2012-05-06 15:45:08 +0530

    Removed bootstrap file as it can occur in the index

--- app/src/bootstrap.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	var booksController = new BooksController($('#content'), new BooksView(), new BooksService());
-	booksController.all();
-	var navigationWidget = new Navigation($('#navigation ul.items'));
-	
-});
