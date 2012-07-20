e3a6676c7c85921b9e2be0d56f55a4c5347f1c86
Henry Lawson <hlawson@thoughtworks.com>
2012-07-20 09:42:52 +0530

    Moved logic out of server and hiding dev test page

---
+++ app/server/index.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var path = require('path');
+var basicRouter = require('./basic.router');
+
+this.start = function(settings) {
+	var app = express.createServer();
+
+	app.configure(function(){
+		app.use(express.methodOverride());
+		app.use(express.bodyParser());
+		app.use(express.static(path.join(settings.currentDir, 'public')));
+		app.use('/src', express.static(path.join(settings.clientDir, 'src')));
+		app.use(app.router);
+		app.use(express.errorHandler({
+			dumpExceptions: false, 
+			showStack: false
+		}));
+		app.get("/", basicRouter.index);
+		app.get("/books", basicRouter.getBooks);
+		app.post("/books", basicRouter.postBooks);
+	});
+
+	if (process.env.DEVELOPMENT) {
+		app.configure('development', function(){
+			app.get('/test', basicRouter.test);
+			app.use('/specs', express.static(path.join(settings.clientDir, 'specs')));
+			app.use('/test/factories', express.static(path.join(settings.clientDir, 'test', 'factories')));
+			app.use(express.errorHandler({
+				dumpExceptions: true, 
+				showStack: true
+			}));
+		});
+	}
+
+	app.listen(settings.port);
+
+	console.log('Running on 127.0.0.1:' + settings.port);
+}
--- server.js
+++ server.js
@@ -1,40 +1,8 @@
-var basicRouter = require('./app/server/basic.router');
-var express = require("express");
+var server = require('./app/server/index');
 var path = require('path');
 
-var settings = {
+server.start({
 	currentDir: path.join(__dirname, 'app'),
 	clientDir: path.join(__dirname, 'app', 'client'),
 	port: (process.env.port || 4567)
-};
-
-var app = express.createServer();
-
-app.configure(function(){
-	app.use(express.methodOverride());
-	app.use(express.bodyParser());
-	app.use(express.static(path.join(settings.currentDir, 'public')));
-	app.use('/src', express.static(path.join(settings.clientDir, 'src')));
-	app.use(app.router);
-	app.use(express.errorHandler({
-		dumpExceptions: false, 
-		showStack: false
-	}));
-	app.get("/", basicRouter.index);
-	app.get("/books", basicRouter.getBooks);
-	app.post("/books", basicRouter.postBooks);
 });
-
-app.configure('development', function(){
-	app.get('/test', basicRouter.test);
-	app.use('/specs', express.static(path.join(settings.clientDir, 'specs')));
-	app.use('/test/factories', express.static(path.join(settings.clientDir, 'test', 'factories')));
-	app.use(express.errorHandler({
-		dumpExceptions: true, 
-		showStack: true
-	}));
-});
-
-app.listen(settings.port);
-
-console.log('Running on 127.0.0.1:' + settings.port);
